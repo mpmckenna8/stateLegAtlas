9709993ca3dabfb6079c5a02411d5d8a88e0c8cc
mpmckenna8 <mpmckenna8@me.com>
2015-05-29 15:28:33 -0700

    Think I have a working thing to make some topojson given a geojson file.

--- geo2topo.js
+++ geo2topo.js
@@ -2,19 +2,71 @@
 
 var topojson = require("topojson");
 var locey = require("./local.js");
+var fs = require("fs")
+
+
+var gogo = {
+          type: "FeatureCollection",
+          features: [
+
+          ]
+        }
+
+
+
 
 
 //console.log()
-//locey(bleeper);
+locey(bleeper);
 function bleeper (geoj){
 var id = 1;
-  console.log('what I try to use is', geoj);
+  console.log('what I try to use is');
   console.log(typeof(geoj));
-  var toppy = topojson.topology(geoj, {"property-transform": function (feature) {
-  return null//feature.properties;
-}});
-  console.log(//Object.keys
-    (toppy)) //.objects.collection))
+  var tose = JSON.parse(geoj);
+  console.log(typeof(tose));
+
+  console.log(tose);
+
+  var bleep = {
+            type: "FeatureCollection",
+            features: [
+
+            ]
+          }
+
+
+console.log(bleep)
+
+console.log(tose.features[2].geometry.coordinates)
+  bleep.features.push(tose.features[0]);
+//bleep.features.push(tose.features[2]);
+
+  var toppy = topojson.topology({colli:tose}, function(d){
+    console.log('in the topo callback')
+  })
+
+
+  for (i in tose.features){
+    //console.log(i)
+
+
+  }
+
+
+
+
+ console.log(  //Object.key
+ (toppy)
+ ) //.objects.collection))
+
+ fs.writeFile('bordop.topojson', JSON.stringify(toppy), function(err, dat){
+   if(err){
+     console.error('somehting was probably dumb');
+   }
+
+   console.log('a bordop file shoulda been made');
+
+ })
 
 }
 
@@ -26,7 +78,7 @@ var practer = {
             {type: "Feature", geometry: {type: "Polygon", coordinates: [[[.5, .6], [.7, .8]]]}}
           ]
         }
-  var toprac = topojson.topology({colli:practer});
+  var toprac = topojson.topology({colli:gogo});
 
   console.log(practer)
   console.log(toprac)
--- local.js
+++ local.js
@@ -6,7 +6,7 @@ module.exports = blah;
 function blah (cb){
   fs.readFile('border.json', 'utf-8', function(err, data){
   //console.log(data.toString());
-  var datj = JSON.parse(data.toString());
+  var datj = JSON.parse(data);
 //  console.log(datj);
   //var nowgeojson = geojson(datj);
 
